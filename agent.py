7e11c0420dba34883cf747fcb4636b6ca82648cd
Abhishek Kori <abhishekkori601@gmail.com>
2026-07-21 15:55:01 +0530

    agent.py

---
+++ agent.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+from pathlib import Path
+
+from dotenv import load_dotenv
+import certifi 
+
+load_dotenv()
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessaage
+from langgraph.graph import StateGraph, START, MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+from tools import tools
