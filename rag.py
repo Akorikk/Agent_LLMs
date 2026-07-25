19e18f58784f9f0370c2993153e0d918ae415a93
Abhishek Kori <abhishekkori601@gmail.com>
2026-07-25 20:34:56 +0530

    rag,py

--- rag.py
+++ rag.py
@@ -35,7 +35,13 @@ vectorstore = Chroma(
 
 def read_file_text(file_path: str) -> str:
     path = Path(file_path) # This converts the string into a Path object. why this important cuz Because a string only stores text. example file_path = "uploads/Resume.PDF" Python only sees:u p l o a d s / R e s u m e . P D F It doesn't know:Where is the filename?What is the extension?Which folder is it in?It's just text.But when you convert it into a Path object, Python now understands it is a file path.
-    suffix = path.suffix.lower()
+    suffix = path.suffix.lower() 
+    """
+When you write path.suffix Python looks at the filename Resume.PDF
+and returns only the extension.
+Output: .PDF Another example:path = Path("report.docx")
+path.suffix returns .docx
+"""
 
     if suffix == ".pdf":
         reader = PdfReader(file_path)
